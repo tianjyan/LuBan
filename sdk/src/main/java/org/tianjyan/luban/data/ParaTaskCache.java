ac48b16f89da041fba26592b1a5b35b79b8c6df8
tianjyan <youngytj@sina.com>
2017-07-27 07:44:12 +0800

    Add consumer classed

---
+++ sdk/src/main/java/org/tianjyan/luban/data/ParaTaskCache.java
@@ -0,0 +1,8 @@
+package org.tianjyan.luban.data;
+
+/**
+ * Created by YOUNG on 2017/7/27.
+ */
+
+public class ParaTaskCache {
+}
---
+++ sdk/src/main/java/org/tianjyan/luban/data/control/ParaTaskConsumer.java
@@ -0,0 +1,8 @@
+package org.tianjyan.luban.data.control;
+
+/**
+ * Created by YOUNG on 2017/7/27.
+ */
+
+public class ParaTaskConsumer {
+}
