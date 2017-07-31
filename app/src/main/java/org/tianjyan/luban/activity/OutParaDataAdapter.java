bf55c3d07a54797512a23da339c38ca1000223cd
tianjyan <youngytj@sina.com>
2017-07-31 18:14:51 +0800

    Remove AbsParaDataAdapter

--- app/src/main/java/org/tianjyan/luban/activity/OutParaDataAdapter.java
+++ app/src/main/java/org/tianjyan/luban/activity/OutParaDataAdapter.java
@@ -2,36 +2,97 @@ package org.tianjyan.luban.activity;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import org.tianjyan.luban.R;
 import org.tianjyan.luban.aidl.AidlEntry;
+import org.tianjyan.luban.aidl.OutPara;
 import org.tianjyan.luban.model.Const;
 
 import java.util.List;
 
-public class OutParaDataAdapter extends AbsParaDataAdapter {
+public class OutParaDataAdapter extends RecyclerView.Adapter {
+    protected List<OutPara> list;
+    protected Context context;
+    protected LayoutInflater inflate;
 
-    public OutParaDataAdapter(Context context, List<? extends AidlEntry> list) {
-        super(context, list);
+    public OutParaDataAdapter(Context context, List<OutPara> list) {
+        this.context = context;
+        this.list = list;
+        this.inflate = LayoutInflater.from(context);
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = null;
         switch (viewType) {
-            case Const.Type_Floating_Title:
-                break;
-            case Const.Type_Normal_Title:
-                break;
-            case Const.Type_Disable_Title:
+            case Const.Type_Title:
+                viewHolder = new TitleViewHolder(inflate.inflate(R.layout.list_view_para_item_title, null));
                 break;
             case Const.Type_Item:
+                viewHolder = new ItemEnableViewHolder(inflate.inflate(R.layout.list_view_para_item, null));
+                break;
+            case Const.Type_Item_Disable:
+                viewHolder = new ItemDisableViewHolder(inflate.inflate(R.layout.list_view_para_item_disable, null));
+                break;
+            default:
                 break;
         }
-        return null;
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        int type;
+        OutPara outPara = list.get(position);
+        switch (outPara.getKey()) {
+            case Const.Floating_Area_Title:
+            case Const.Normal_Area_Title:
+            case Const.Disable_Area_Title:
+                type = Const.Type_Title;
+                break;
+            default:
+                if (outPara.getDisplayProperty() == AidlEntry.DISPLAY_DISABLE) {
+                    type = Const.Type_Item_Disable;
+                } else {
+                    type = Const.Type_Item;
+                }
+                break;
+        }
+
+        return type;
+    }
+
+    public class ItemEnableViewHolder extends RecyclerView.ViewHolder {
+
+        public ItemEnableViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class ItemDisableViewHolder extends RecyclerView.ViewHolder {
+
+        public ItemDisableViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class TitleViewHolder extends RecyclerView.ViewHolder {
+
+        public TitleViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
--- app/src/main/java/org/tianjyan/luban/model/Const.java
+++ app/src/main/java/org/tianjyan/luban/model/Const.java
@@ -5,9 +5,7 @@ public class Const {
     public static final String Normal_Area_Title = "Normal Area";
     public static final String Disable_Area_Title = "Disable Area";
 
-    public static final int Type_Floating_Title = 0;
-    public static final int Type_Normal_Title = 1;
-    public static final int Type_Disable_Title = 2;
-    public static final int Type_Item = 3;
-    public static final int Type_Count = 4;
+    public static final int Type_Title = 0;
+    public static final int Type_Item = 1;
+    public static final int Type_Item_Disable = 2;
 }
