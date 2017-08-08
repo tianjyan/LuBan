784caf786a211158f72bbecdda435960f5c5cc34
tianjyan <youngytj@sina.com>
2017-08-08 15:50:11 +0800

    Log

--- app/src/main/java/org/tianjyan/luban/manager/LogManager.java
+++ app/src/main/java/org/tianjyan/luban/manager/LogManager.java
@@ -2,10 +2,37 @@ package org.tianjyan.luban.manager;
 
 import org.tianjyan.luban.model.LogEntry;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class LogManager {
     private Thread thread;
     private boolean flag;
     private LinkedBlockingDeque<LogEntry> queue = new LinkedBlockingDeque<>(1000);
+
+    private static LogManager INSTANCE = new LogManager();
+    public static LogManager getInstance() {
+        return INSTANCE;
+    }
+
+    public LogManager() {
+        thread = new Thread(() -> {
+           try {
+               while (flag) {
+                   Thread.sleep(1000);
+                   List<LogEntry> tempList = new ArrayList<>();
+                   queue.drainTo(tempList);
+                   // TODO: 发送到UI
+               }
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           };
+        });
+        thread.start();
+    }
+
+    public void log(long tid, int level, String tag, String msg) {
+        queue.offer(new LogEntry(tid, level, tag, msg));
+    }
 }
--- app/src/main/java/org/tianjyan/luban/model/LogEntry.java
+++ app/src/main/java/org/tianjyan/luban/model/LogEntry.java
@@ -1,5 +1,16 @@
 package org.tianjyan.luban.model;
 
 public class LogEntry {
+    private long tid;
+    private int level;
+    private String tag;
+    private String msg;
+    private String displayTime;
 
+    public LogEntry(long tid, int level, String tag, String msg) {
+        this.tid = tid;
+        this.level = level;
+        this.tag = tag;
+        this.msg = msg;
+    }
 }
